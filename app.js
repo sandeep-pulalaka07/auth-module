f36d9034e60c7c53072657dfd20e6ce182b077e9
Yadagiri Talada <yadagiri_talada@epam.com>
2024-06-27 19:33:32 +0530

    Added configuration for ENV

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
-const HOST = 'localhost';
+const port = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, HOST, () => {
-    console.log('listening on port: ' + PORT);
+app.listen(port, () => {
+    console.log(`Server is running at  http://localhost:${port}`);
 });
