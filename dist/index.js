366305e7ff84bc32da1d9fcfb7bc8ca75d179534
Yadagiri Talada <yadagiri_talada@epam.com>
2024-06-28 19:55:43 +0530

    Added cors to app

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    exposedHeaders: ["*"]
+}));
 app.listen(port, () => {
     console.log(`Server is running at  http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -7,6 +8,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({
+    exposedHeaders: ["*"]
+}));
 
 app.listen(port, () => {
     console.log(`Server is running at  http://localhost:${port}`);
